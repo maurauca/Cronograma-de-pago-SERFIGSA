f4b74729a93af6f8976bda4df97fcb26bf992b7f
StephFlooo <stephany.flores15893@est.uca.edu.ni>
2023-05-21 15:27:55 -0600

    se mandan a llamar las funciones del archivo cliente.cpp en principal

--- Codigo/principal.cpp
+++ Codigo/principal.cpp
@@ -1,4 +1,21 @@
-//Sistema de pago crediticio SERFIGSA
 #include <iostream>
-#include "funciones.cpp"
+#include "cliente.cpp"
 using namespace std;
+
+int main(int argc, char const *argv[])
+{
+void addClient(cliente cl);
+void showClient(int pos);
+void showClients();
+int isClient(char cedula[]);
+void startClient(int pos);
+cliente getClient(int pos);
+void updateClient(cliente cl, int pos);
+void deleteClient(int pos);
+void saveClientes();
+void readClientes();    
+
+return 0;
+}
+
+
