51e7ffc397a6ceff45fa51858e8a51d29b70b32d
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-05-22 20:58:48 -0600

    Se compila el primer borrador de codigo final (solo registros de clientes)

--- Codigo/principal.cpp
+++ Codigo/principal.cpp
@@ -4,17 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-void addClient(cliente cl);
-void showClient(int pos);
-void showClients();
-int isClient(char cedula[]);
-void startClient(int pos);
-cliente getClient(int pos);
-void updateClient(cliente cl, int pos);
-void deleteClient(int pos);
-void saveClientes();
-void readClientes();    
-
+start();
 return 0;
 }
 
